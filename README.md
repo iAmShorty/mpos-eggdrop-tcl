8260d7bff9107f5054a37e6db115aa0e84fae4b2
root <rafta@gmx.net>
2013-10-30 14:44:50 +0100

    [UPDATE] all stats will fetch data from api, except last. will do it later

--- README.md
+++ README.md
@@ -5,32 +5,24 @@ MPOS Addon - TCL Script for Eggdrop IRC Bot
 
 This script is an addon for the popular eggdrop <a href="http://www.eggheads.org">Bot</a>
 
-and gets Data from <a href="https://github.com/TheSerapher/php-mpos">MPOS</a>, a fantastic Webinterface for people running an Cryptocoin Pool
-
+and gets API Data from <a href="https://github.com/TheSerapher/php-mpos">MPOS</a>, 
+a fantastic Webinterface for people running Cryptocoin Pools
 
 
 ================
 
 Usage: 
 
-!mininginfo  - Blockstats
-
-!pool        - Pool Information
-
-!last        - Information about last found Block
-
-!user <user> - Information about a specific User
+!block       - Blockstats"
+!pool        - Pool Information"
+!last        - Information about last found Block"
+!user <user> - Information about a specific User"
+!help        - This help text"
        
-================
-
-This Script uses TCL and <a href="http://www.fastbase.co.nz/fbsql/index.html">fbsql</a>
-
-fbsql is needed for executing MySQL Queries against the MPOS Database
 
 ================
 
 Requirements:
 
  - TCL (with JSON Support)
- - MySQL Client Libraries
- - Fastbase MySQL
+
--- poolstats.tcl
+++ poolstats.tcl
@@ -3,35 +3,34 @@
 set scriptversion "v0.1 ALPHA"
 set help_blocktime "10"
 
-load ./modules/fbsql.so
-
-set sqluser "sqluser"
-set sqlpass "sqlpass"
-set sqlhost "sqlhost"
-set sqldb "sqldb"
+set apiurl "http://yourdomain.td/"
+set apikey "YOURAPIKEYFROMMPOS"
 
 
-bind pub - !mininginfo mining_info
 bind pub - !pool pool_info
+bind pub - !block block_info
 bind pub - !last last_info
 bind pub - !user user_info
 bind pub - !help printUsage
 
 
 proc printUsage {nick host hand chan arg} {
-    putquick "NOTICE $nick :Usage: !mininginfo  - Blockstats"
+    putquick "NOTICE $nick :Usage: !block       - Blockstats"
     putquick "NOTICE $nick :       !pool        - Pool Information"
     putquick "NOTICE $nick :       !last        - Information about last found Block"
     putquick "NOTICE $nick :       !user <user> - Information about a specific User"
+    putquick "NOTICE $nick :       !help        - This help text"
 }
 
 
 
-proc user_info {nick host hand chan arg} {
- 	global sqluser sqlpass sqlhost sqldb help_blocktime help_blocked
- 	sql connect $sqlhost $sqluser $sqlpass
- 	sql selectdb $sqldb
 
+
+proc user_info {nick host hand chan arg} {
+ 	global apiurl apikey help_blocktime help_blocked
+ 	
+ 	set action "index.php?page=api&action=getuserstatus&id=$arg&api_key="
+ 	
  	set mask [string trimleft $host ~]
  	regsub -all {@([^\.]*)\.} $mask {@*.} mask	 	
  	set mask *!$mask
@@ -42,14 +41,41 @@ proc user_info {nick host hand chan arg} {
     	  return
   	}
   	
+  	set newurl $apiurl
+  	append newurl $action
+  	append newurl $apikey
   	
-  	set UserID [sql query "select id from accounts WHERE username = '$arg'"]
-	set Shares_valid [sql query "select SUM(valid) from statistics_shares WHERE account_id = '$UserID'"]
-  	set Shares_invalid [sql query "select SUM(invalid) from statistics_shares WHERE account_id = '$UserID'"]
-  	
-	putquick "PRIVMSG $chan :Username: $arg | Hashrate: 300 kh/s | Shares Valid: $Shares_valid | Shares Invalid: $Shares_invalid "
-
- 	sql disconnect
+    set token [::http::geturl "$newurl"]
+    set data [::http::data $token]
+    ::http::cleanup $token
+    #putlog "xml: $data"
+    set results [::json::json2dict $data]
+    
+    #putlog $results
+    
+	foreach {key value} $results {
+		foreach {sub_key sub_value} $value {
+			if {$sub_key eq "data"} {
+				#putlog "Sub: $sub_value"
+				foreach {elem elem_val} $sub_value {
+					#putlog "Ele: $elem - Val: $elem_val"
+					
+					foreach {subelem subelem_val} $elem_val {
+						#putlog "SubEle: $subelem - SubVal: $subelem_val"
+						
+						if {$subelem eq "valid"} { set user_validround "Valid this round: $subelem_val" }
+						if {$subelem eq "invalid"} { set user_invalidround "Invalid this round: $subelem_val" }
+						
+					}
+					
+					if {$elem eq "hashrate"} { set user_hashrate "Hashrate: $elem_val" }
+					if {$elem eq "sharerate"} { set user_sharerate "Sharerate: $elem_val" }
+				}
+			}
+		}
+	}
+	
+	putquick "PRIVMSG $chan :Username: $arg | $user_hashrate kh/s | $user_validround | $user_invalidround | $user_sharerate"
 
 	set help_blocked($mask) 1
 	utimer $help_blocktime [ list unset help_blocked($mask) ]
@@ -69,7 +95,6 @@ proc last_info {nick host hand chan arg } {
  
   	if {[info exists help_blocked($mask)]} {
     	  putquick "NOTICE $nick : You have been blocked for $help_blocktime Seconds, please be patient..."
-    	  sql disconnect
     	  return
   	}
 
@@ -90,66 +115,124 @@ proc last_info {nick host hand chan arg } {
 
 }
 
-proc mining_info {nick host hand chan arg } {
- 	global sqluser sqlpass sqlhost sqldb help_blocktime help_blocked
- 	sql connect $sqlhost $sqluser $sqlpass
- 	sql selectdb $sqldb
 
- 	set mask [string trimleft $host ~]
- 	regsub -all {@([^\.]*)\.} $mask {@*.} mask	 	
- 	set mask *!$mask
- 
-  	if {[info exists help_blocked($mask)]} {
-    	  putquick "NOTICE $nick : You have been blocked for $help_blocktime Seconds, please be patient..."
-    	  sql disconnect
-    	  return
-  	}
-
-	set countblocks [sql query "select count(ID) from blocks"]
-	set countorphanblocks [sql query "select count(ID) from blocks where confirmations = -1"]
-	set countvalidblocks [sql query "select count(ID) from blocks where confirmations > 0"]
-
-	putquick "PRIVMSG $chan : Found Blocks: $countblocks | Orphaned: $countorphanblocks | Valid: $countvalidblocks"
-
- 	sql disconnect
-
-	set help_blocked($mask) 1
-	utimer $help_blocktime [ list unset help_blocked($mask) ]
-
-}
 
 # Pool Stats
 proc pool_info {nick host hand chan arg} {
+    global apiurl apikey
 	package require http
 	package require json
-
+	
+	set action "index.php?page=api&action=getpoolstatus&api_key="
+	
  	set mask [string trimleft $host ~]
  	regsub -all {@([^\.]*)\.} $mask {@*.} mask	 	
  	set mask *!$mask
  
   	if {[info exists help_blocked($mask)]} {
     	  putquick "NOTICE $nick : You have been blocked for $help_blocktime Seconds, please be patient..."
-    	  sql disconnect
+    	  return
+  	}
+
+  	set newurl $apiurl
+  	append newurl $action
+  	append newurl $apikey
+  	
+    set token [::http::geturl "$newurl"]
+    set data [::http::data $token]
+    ::http::cleanup $token
+    #putlog "xml: $data"
+    set results [::json::json2dict $data]
+
+	foreach {key value} $results {
+		foreach {sub_key sub_value} $value {
+			putlog "Sub: $sub_key - $sub_value"
+			if {$sub_key eq "runtime"} { set pool_runtime "Runtime: $sub_value" }
+			
+			if {$sub_key eq "data"} {
+				#putlog "Sub: $sub_value"
+				foreach {elem elem_val} $sub_value {
+					#putlog "Ele: $elem - Val: $elem_val"
+				
+      				if {$elem eq "hashrate"} { set pool_hashrate "Hashrate: $elem_val" }
+      				if {$elem eq "efficiency"} { set pool_efficiency "Efficiency: $elem_val" } 
+      				if {$elem eq "workers"} { set pool_workers "Workers: $elem_val" } 
+      				if {$elem eq "nethashrate"} { set pool_nethashrate "Net Hashrate: $elem_val khash" } 
+				
+				}
+			}
+		}
+	}
+
+    #putlog $results
+    putquick "PRIVMSG $chan :$pool_runtime | $pool_hashrate khash | $pool_efficiency | $pool_workers | $pool_nethashrate"
+
+}
+
+
+
+# Block Stats
+proc block_info {nick host hand chan arg} {
+    global apiurl apikey
+	package require http
+	package require json
+	
+	set action "index.php?page=api&action=getpoolstatus&api_key="
+	
+ 	set mask [string trimleft $host ~]
+ 	regsub -all {@([^\.]*)\.} $mask {@*.} mask	 	
+ 	set mask *!$mask
+ 
+  	if {[info exists help_blocked($mask)]} {
+    	  putquick "NOTICE $nick : You have been blocked for $help_blocktime Seconds, please be patient..."
     	  return
   	}
   	
-    set token [::http::geturl "http://yourownurl.com/index.php?page=api&action=public"]
+  	set newurl $apiurl
+  	append newurl $action
+  	append newurl $apikey
+  	
+    set token [::http::geturl "$newurl"]
     set data [::http::data $token]
     ::http::cleanup $token
     #putlog "xml: $data"
     set results [::json::json2dict $data]
     
-    set pool_name [dict get $results pool_name]
-    set pool_hashrate [dict get $results hashrate]
-    set pool_workers [dict get $results workers]
-    set pool_lastblock [dict get $results last_block]
-    set pool_roundshares [dict get $results shares_this_round]
+    #putlog $results
     
-    putlog $results
-    putquick "PRIVMSG $chan :Pool Name: $pool_name"
-    putquick "PRIVMSG $chan :Hashrate: $pool_hashrate khash | Workers: $pool_workers | Last Block: $pool_lastblock | Shares this round: $pool_roundshares"
+	foreach {key value} $results {
+		foreach {sub_key sub_value} $value {
+			if {$sub_key eq "data"} {
+				#putlog "Sub: $sub_value"
+				foreach {elem elem_val} $sub_value {
+					#putlog "Ele: $elem - Val: $elem_val"
 
+      				if {$elem eq "currentnetworkblock"} { set block_current "Current Block: $elem_val" } 
+      				if {$elem eq "nextnetworkblock"} { set block_next "Next Block: $elem_val" } 
+      				if {$elem eq "lastblock"} { set block_last "Last Block: $elem_val" }
+      				if {$elem eq "networkdiff"} { set block_diff "Difficulty: $elem_val" } 
+      				if {$elem eq "esttime"} { set block_time "Est. Time to resolve: $elem_val" } 
+      				if {$elem eq "estshares"} { set block_shares "Est. Shares to resolve: $elem_val" } 
+      				if {$elem eq "timesincelast"} { 
+      					set timediff [expr {$elem_val / 60}]
+      					#set timediff $elem_val
+      					set block_timelast "Time since last Block: $timediff minutes"
+      				}
+				
+				}
+			}
+		}
+	}
+
+    putquick "PRIVMSG $chan :$block_current | $block_next | $block_last | $block_diff | $block_time | $block_shares | $block_timelast | $block_last"
+	#putquick "PRIVMSG $chan :$sub_value"
 }
+
+
+
+
+
+
 
 
 putlog "===>> Mining-Pool-Stats - Version $scriptversion - geladen"
