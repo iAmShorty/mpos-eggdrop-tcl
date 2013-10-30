a9d2f35037ddb1fd1229070ea05ce3631552f71c
root <rafta@gmx.net>
2013-10-30 22:48:16 +0100

    [UPDATE] adding roundstats

--- poolstats.tcl
+++ poolstats.tcl
@@ -11,6 +11,7 @@ bind pub - !pool pool_info
 bind pub - !block block_info
 bind pub - !last last_info
 bind pub - !user user_info
+bind pub - !round round_info
 bind pub - !help printUsage
 
 
@@ -18,6 +19,7 @@ bind pub - !help printUsage
 proc printUsage {nick host hand chan arg} {
     putquick "NOTICE $nick :Usage: !block       - Blockstats"
     putquick "NOTICE $nick :       !pool        - Pool Information"
+    putquick "NOTICE $nick :       !round       - Round Information"
     putquick "NOTICE $nick :       !last        - Information about last found Block"
     putquick "NOTICE $nick :       !user <user> - Information about a specific User"
     putquick "NOTICE $nick :       !help        - This help text"
@@ -64,19 +66,88 @@ proc user_info {nick host hand chan arg} {
 						
 					}
 					
-					if {$elem eq "hashrate"} { set user_hashrate "Hashrate: $elem_val" }
-					if {$elem eq "sharerate"} { set user_sharerate "Sharerate: $elem_val" }
+					if {$elem eq "hashrate"} { set user_hashrate "Hashrate: $elem_val kh/s" }
+					if {$elem eq "sharerate"} { set user_sharerate "Sharerate: $elem_val S/s" }
 				}
 			}
 		}
 	}
 	
-	putquick "PRIVMSG $chan :Username: $arg | $user_hashrate kh/s | $user_validround | $user_invalidround | $user_sharerate"
+	putquick "PRIVMSG $chan :User Info for $arg"
+	putquick "PRIVMSG $chan :$user_hashrate | $user_validround | $user_invalidround | $user_sharerate"
 
 	set help_blocked($mask) 1
 	utimer $help_blocktime [ list unset help_blocked($mask) ]
 
 }
+
+
+
+proc round_info {nick host hand chan arg } {
+ 	global apiurl apikey help_blocktime help_blocked
+	package require http
+	package require json
+	
+ 	set action "index.php?page=api&action=getdashboarddata&api_key="
+ 	
+ 	set mask [string trimleft $host ~]
+ 	regsub -all {@([^\.]*)\.} $mask {@*.} mask	 	
+ 	set mask *!$mask
+ 
+  	if {[info exists help_blocked($mask)]} {
+    	  putquick "NOTICE $nick : You have been blocked for $help_blocktime Seconds, please be patient..."
+    	  return
+  	}
+
+  	set newurl $apiurl
+  	append newurl $action
+  	append newurl $apikey
+  	
+    set token [::http::geturl "$newurl"]
+    set data [::http::data $token]
+    ::http::cleanup $token
+    #putlog "xml: $data"
+    set results [::json::json2dict $data]
+	
+	foreach {key value} $results {
+		#putlog "Key: $key - $value"
+		foreach {sub_key sub_value} $value {
+			#putlog "Sub: $sub_key - $sub_value"
+			foreach {elem elem_val} $sub_value {
+				#putlog "Ele: $elem - Val: $elem_val"
+				if {$elem eq "pool"} {
+					#putlog "Ele: $elem - Val: $elem_val"
+					foreach {elem2 elem_val2} $elem_val {
+						#putlog "Ele: $elem2 - Val: $elem_val2"
+						if {$elem2 eq "shares"} {
+							foreach {elem3 elem_val3} $elem_val2 {
+								#putlog "Ele: $elem3 - Val: $elem_val3"
+								
+								if {$elem3 eq "valid"} { set shares_valid "$elem_val3" }
+								if {$elem3 eq "invalid"} { set shares_invalid "$elem_val3" }
+								if {$elem3 eq "estimated"} { set shares_estimated "Estimated Shares: $elem_val3" }
+								if {$elem3 eq "progress"} { set shares_progress "Progress: $elem_val3 %" }
+								
+							}
+						}
+					}				
+				}
+			}
+		}
+	}
+	
+	set allshares [expr $shares_valid+$shares_invalid]
+	
+	putquick "PRIVMSG $chan :Actual Round"
+ 	putquick "PRIVMSG $chan :$shares_estimated | Sharecount: $allshares | Shares valid: $shares_valid | Shares invalid: $shares_invalid | $shares_progress"
+
+	set help_blocked($mask) 1
+	utimer $help_blocktime [ list unset help_blocked($mask) ]
+
+}
+
+
+
 
 
 
@@ -135,6 +206,7 @@ proc last_info {nick host hand chan arg } {
 		}
 	}
 	
+	putquick "PRIVMSG $chan :Last Block"
  	putquick "PRIVMSG $chan :$last_block | $last_confirmed | $last_difficulty | $last_timefound | $last_shares | $last_estshares | $last_finder"
 
 	set help_blocked($mask) 1
@@ -173,26 +245,24 @@ proc pool_info {nick host hand chan arg} {
 
 	foreach {key value} $results {
 		foreach {sub_key sub_value} $value {
-			putlog "Sub: $sub_key - $sub_value"
-			if {$sub_key eq "runtime"} { set pool_runtime "Runtime: $sub_value" }
-			
+			#putlog "Sub: $sub_key - $sub_value"
 			if {$sub_key eq "data"} {
 				#putlog "Sub: $sub_value"
 				foreach {elem elem_val} $sub_value {
 					#putlog "Ele: $elem - Val: $elem_val"
 				
-      				if {$elem eq "hashrate"} { set pool_hashrate "Hashrate: $elem_val" }
-      				if {$elem eq "efficiency"} { set pool_efficiency "Efficiency: $elem_val" } 
+      				if {$elem eq "hashrate"} { set pool_hashrate "Hashrate: $elem_val kh/s" }
+      				if {$elem eq "efficiency"} { set pool_efficiency "Efficiency: $elem_val %" } 
       				if {$elem eq "workers"} { set pool_workers "Workers: $elem_val" } 
-      				if {$elem eq "nethashrate"} { set pool_nethashrate "Net Hashrate: $elem_val khash" } 
+      				if {$elem eq "nethashrate"} { set pool_nethashrate "Net Hashrate: $elem_val kh/s" } 
 				
 				}
 			}
 		}
 	}
 
-    #putlog $results
-    putquick "PRIVMSG $chan :$pool_runtime | $pool_hashrate khash | $pool_efficiency | $pool_workers | $pool_nethashrate"
+    putquick "PRIVMSG $chan :Pool Stats"
+    putquick "PRIVMSG $chan :$pool_hashrate | $pool_efficiency | $pool_workers | $pool_nethashrate"
 
 }
 
@@ -256,8 +326,9 @@ proc block_info {nick host hand chan arg} {
 		}
 	}
 
+    putquick "PRIVMSG $chan :Block Stats"
     putquick "PRIVMSG $chan :$block_current | $block_next | $block_last | $block_diff | $block_time | $block_shares | $block_timelast | $block_last"
-	#putquick "PRIVMSG $chan :$sub_value"
+
 }
 
 
