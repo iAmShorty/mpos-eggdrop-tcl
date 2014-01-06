a5a3f6185abab7b61a2c3eec766283598a548893
root <rafta@gmx.net>
2014-01-06 14:56:51 +0100

    [UPDATE] cleanup

--- marketdata.tcl
+++ marketdata.tcl
@@ -1,5 +1,5 @@
 #
-# Cryptsy Market Data
+# Cryptocoin Market Data
 #
 #
 
@@ -34,11 +34,6 @@ proc price_info {nick host hand chan arg} {
   	
   	set newurl $marketapi
   	
-    set trade_price "0"
-    set trade_trime "0"
-    set trade_label "0"
-    set trade_volume "0"
-    
     if {$activemarket eq "1"} {
     	set market_name "Coins-E"
     } elseif {$activemarket eq "2"} {
@@ -212,4 +207,4 @@ proc market_cryptsy {chan marketdataresult} {
 
 
 
-putlog "===>> Cryptsy Market Data - Version $scriptversion loaded"
+putlog "===>> Market Data - Version $scriptversion loaded"
